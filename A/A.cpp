df47b0c20cf48a5eac67a7ed2b0511f31761bb4b
Evgeniy Ivanov <timeescapenow@gmail.com>
2019-07-08 19:04:47 +0300

    1573

--- A/A.cpp
+++ A/A.cpp
@@ -9,10 +9,49 @@
 #include <string>
 
 #define MIN(a,b) (((a) < (b))?(a):(b))
+#define MAX(a,b) (((a) > (b))?(a):(b))
 
 using namespace std;
 
 typedef long long int64;
+
+
+struct ST
+{
+	vector<int> base;
+
+	void Init(vector<int>& a)
+	{
+		base.assign(4 * a.size(), 0);
+		Build(a, 1, 0, a.size() - 1);
+	}
+
+	void Build(vector<int>& a, int v, int tl, int tr) {
+		if (tl == tr)
+			base[v] = a[tl];
+		else {
+			int tm = (tl + tr) / 2;
+			Build(a, v * 2, tl, tm);
+			Build(a, v * 2 + 1, tm + 1, tr);
+			base[v] = MAX(base[v * 2], base[v * 2 + 1]);
+		}
+	}
+	
+	int Max(int v, int tl, int tr, int l, int r)
+	{
+		if (l > r)
+		{
+			return 0;
+		}
+		if (tl == l && tr == r)
+		{
+			return base[v];
+		}
+		int tm = (tl + tr) / 2;
+		return MAX(Max(2 * v, tl, tm, l, MIN(r, tm)), Max(2 * v + 1, tm + 1, tr, MAX(l, tm + 1), r));
+	}
+
+};
 
 void P(int64& x, int64& y)
 {
@@ -42,23 +81,29 @@ int main()
 	cout << a << ' ' << b << endl;
 	return 0;
 	*/
-	int n, s;
-	cin >> n >> s;
-	vector<int> d(n);
-	for (int i = 0; i < n; i++)
+	int b, r, y;
+	cin >> b >> r >> y;
+	int k;
+	cin >> k;
+	int res = 1;
+	for (int i = 0; i < k; i++)
 	{
-		cin >> d[i];
+		string a;
+		cin >> a;
+		if (a == "Red")
+		{
+			res *= r;
+			continue;
+		}
+		if (a == "Blue")
+		{
+			res *= b;
+			continue;
+		}
+		if (a == "Yellow")
+		{
+			res *= y;
+		}
 	}
-	vector<int> a(n);
-	for (int i = 1; i < n; i++)
-	{
-		a[i] = d[i - 1] / d[i];
-		s /= a[i];
-	}
-	a[0] = s;
-	for (int i = 0; i < n; i++)
-	{
-		cout << a[i] - 1 << ' ';
-	}
-	cout << endl;
+	cout << res << endl;
 }
